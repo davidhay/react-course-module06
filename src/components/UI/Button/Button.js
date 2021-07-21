5344ec123a18f879c47a823c647c1b7a9fd78c73
David Hay <david.j.hay@gmail.com>
2021-07-21 11:56:44 +0100

    post lesson 79 - css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,6 +1,8 @@
-import styled from "styled-components";
+import React from "react";
+import styles from "./Button.module.css";
+//import styled from "styled-components";
 
-const Button = styled.button`
+/*const Button = styled.button`
   font: inherit;
   padding: 0.5rem 1.5rem;
   border: 1px solid #8b005d;
@@ -25,5 +27,12 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
-
+*/
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 export default Button;
